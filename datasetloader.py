finally fixed normalization issues

datasetloader.py
@@ -22,7 +22,7 @@ import random
 
 # 5-Channel Loader
 class ISLES2018_loader(Dataset):
-    def __init__(self, file_dir, modalities=None):
+    def __init__(self, file_dir, modalities=None,validate=False):
         super().__init__()
         
         self.samples = []
@@ -46,22 +46,59 @@ class ISLES2018_loader(Dataset):
                     if modality != 'OT':                # ignore the ground truth
 
                         # add image augmentations here 
+                        #print(np.max(case[modality].get_fdata()), np.min(case[modality].get_fdata()))
 
-                        slice = case[modality].get_fdata()[:,:,i]
+                        slice = self.normalizeIntensityRange(case[modality].get_fdata()[:,:,i])
+
+                        print(np.max(slice), np.min(slice))
                         
+                        """
+                        slice=case[modality].get_fdata()
+                        img_array = np.array(slice).astype('float64')
+                        img_2d = img_array[:,:,i].transpose((1,0))
+                        img_2d = np.uint8(img_2d[None,:])
+                        img_2d = torch.from_numpy(img_2d)
+
+                        image = TF.to_pil_image(img_2d)
+                        image = TF.to_tensor(image)
+                        """
+                        #print(img_2d.shape)
                         #slice = self.n4BiasCorrection(slice)
                         
                         image_slice = torch.from_numpy(slice).float().unsqueeze(0) # image slice converted to torch tensor
                         
+
                         # normalize image
-                        image_slice = normalize(image_slice)
+                        #image_slice = normalize(image_slice)
+                        plt.imshow(image_slice.squeeze(0), cmap="gray")
+                        plt.colorbar(label='intensity')
+                        plt.show()
+
 
                         slices.append(image_slice) # add the slice to the array
                 
                 gt_slice = torch.from_numpy(case['OT'].get_fdata()[:,:,i]).float().unsqueeze(0) # slice of the corresponding ground_truths
-                
-                gt_slice = normalize(gt_slice)
 
+                """
+                gt_slice=case['OT'].get_fdata()
+                gt_array = np.array(gt_slice)
+                gt_2d = gt_array[:,:,i].transpose((1,0))
+
+                gt_2d = np.uint8(gt_2d[None,:])
+                gt_2d = torch.from_numpy(gt_2d)
+
+                gt = TF.to_pil_image(gt_2d)
+                gt = TF.to_tensor(gt)
+                """
+                
+
+                #plt.imshow(gt.squeeze(0), cmap="gray")
+                #plt.colorbar(label='intensity')
+                #plt.show()
+                
+                #gt_slice = normalize(gt_slice)
+                #plt.imshow(gt_slice.squeeze(0), cmap="gray")
+                #plt.show()
                 # now transform all the slices in the array before concatenating them:
                 # slices, gt_slice = self.transform(slices,gt_slice)
 
@@ -114,6 +151,9 @@ class ISLES2018_loader(Dataset):
     
         return t_img
     
+    def normalizeIntensityRange(self,data):
+        return (data - np.min(data)) / (np.max(data) - np.min(data))
+    
     # this bit is a huge bottleneck
     def n4BiasCorrection(self, img):
         
@@ -131,9 +171,9 @@ class ISLES2018_loader(Dataset):
             
             
 
-directory = "ISLES/TRAINING"
-modalities = ['OT', 'CT', 'CT_CBV', 'CT_CBF', 'CT_Tmax' , 'CT_MTT']
-dataset = ISLES2018_loader(directory, modalities)
+#directory = "ISLES/TRAINING"
+#modalities = ['OT', 'CT', 'CT_CBV', 'CT_CBF', 'CT_Tmax' , 'CT_MTT']
+#dataset = ISLES2018_loader(directory, modalities)
 #print(dataset.__len__())
 
 

train.py
@@ -14,9 +14,10 @@ import torchio as tio
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.utils import make_grid
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
-from utils import DiceLoss
+from utils import DiceLoss, check_accuracy
 
 # hyperparameters
 LEARNING_RATE = 1e-4
@@ -29,6 +30,19 @@ IMAGE_WIDTH = 256
 PIN_MEMORY = True
 LOAD_MODEL = False
 
+def show_batch(dl):
+    for images, labels in dl:
+        print(len(images))
+        print(images[0].shape)
+        
+        image = images[0].permute(1,2,0)
+        label = labels[0]
+
+        plt.imshow(label.squeeze(0), cmap="gray")
+        
+        plt.show()
+        
+
 
 def train_model(model,loaders,optimizer,num_of_epochs,loss_fn):
     
@@ -38,6 +52,7 @@ def train_model(model,loaders,optimizer,num_of_epochs,loss_fn):
 
         # iterate through the batches
         for i, data in enumerate(loaders[0], 0):
+
             # put images into devices
             train_image, ground_truth = data[0].to(DEVICE), data[1].to(DEVICE)
             optimizer.zero_grad()
@@ -52,29 +67,30 @@ def train_model(model,loaders,optimizer,num_of_epochs,loss_fn):
 
             running_loss += loss.item()
 
-           
-        print("loss for epoch " + str(epoch) + ":  " + str(running_loss))
+            if i % 25 == 0:    
+                print('[%d, %5d] loss: %.3f' % (epoch, i, running_loss / 25))
+                running_loss = 0.0
             
 
 
 if __name__ == '__main__':
+
     unet_2d = UNet() # make sure to change the number of channels in the unet model file
     print(DEVICE)
     unet_2d.to(DEVICE)
 
     train_directory = "ISLES/TRAINING"
-    #test_directory = "ISLES/TESTING"
+    test_directory = "ISLES/TESTING"
 
     modalities = ['OT', 'CT', 'CT_CBV', 'CT_CBF', 'CT_Tmax' , 'CT_MTT']
-    train_dataset = ISLES2018_loader(train_directory, modalities)
-    #test_dataset = ISLES2018_loader(test_directory, modalities)
 
-    # split dataset into train/val for given modality
-    #print(len(train_dataset))
+    train_dataset = ISLES2018_loader(train_directory, modalities)
+    print("Loaded Training Data")
+
+    #test_dataset = ISLES2018_loader(test_directory, modalities)
+    #print("Loaded Test Data")
 
     train_set, val_set = random_split(train_dataset, (400,102))
-    #print(len(train_set[1]))
-    #print(len(train_set),len(val_set))
 
     # need to figure out how to import the data without issues with masks etc
     train_dl = DataLoader(train_set, batch_size=BATCH_SIZE, num_workers=4 ,shuffle=True, pin_memory=True)
@@ -87,4 +103,5 @@ if __name__ == '__main__':
 
     loss_fn = nn.BCEWithLogitsLoss()
 
-    train_model(unet_2d, (train_dl, valid_dl),optimizer,100,loss_fn)
+    #show_batch(train_dl)
+    #train_model(unet_2d, (train_dl, valid_dl),optimizer,100,dsc_loss)

utils.py
@@ -38,10 +38,10 @@ def check_accuracy(loader, model, device="cuda"):
     dice_score = 0
     model.eval()
 
-    with torch.no_grad():       # we want to compare the mask and the predictions together/ for binary
+    with torch.no_grad():       # we want to compare the mask and the predictions together / for binary
         for x, y in loader:
             x = x.to(device)
-            y = y.to(device).unsqueeze(1)
+            y = y.to(device)
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
             num_correct += (preds == y).sum()